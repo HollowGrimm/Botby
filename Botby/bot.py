0c1b665ebdcbe9bf4bd42ecfa35a1942346a9b39
Bobby <bobby.nguyen000@gmail.com>
2021-06-30 20:44:50 -0700

    1.5.3
    Lowered percentage chance of some events
    Prevented bots from activating events
    Added "roll" command

--- Botby/bot.py
+++ Botby/bot.py
@@ -28,6 +28,22 @@ async def help(ctx):
     """This function does does not help"""
     await ctx.send("No.")
 
+@bot.command()
+async def roll(ctx, *, dice: str):
+    if dice.lower().strip()[0] == "d":
+        n = int(dice.strip()[1] + dice.strip()[2])
+        r = random.randint(1, n)
+
+        await ctx.send("You rolled: " + str(r))
+
+    '''if dice.lower().strip()[1] == "d":
+        n = int(dice.strip()[2] + dice.strip()[3])
+        rolls = []
+        for i in range(int(dice.lower().strip()[0])):
+            rolls[i] = random.randint(1, n)
+
+        await ctx.send("You rolled " + rolls)'''
+
 '''@bot.command()
 async def command(ctx, *, command):
     await ctx.send("No.")'''
@@ -41,27 +57,28 @@ async def on_message(message):
 
     message_string = message.content.lower()
 
-    if message_string.strip()[0] != cp:
-        if message_string.strip()[0] == cp:
-            print(f"@{author_name} with ID: {author_id} sent a command.")
-        else:
-            print(f"@{author_name} with ID: {author_id} sent a message.")
-    
+    if message_string.strip()[0] == cp:
+        print(f"@{author_name} with ID: {author_id} sent a command.")
+    else:
+        print(f"@{author_name} with ID: {author_id} sent a message.")
+
+    if message.author == bot.user or message.author.bot:
+        return
+    elif message_string.strip()[0] != cp:
         if message_string.strip()[-4:] == "lies" and author_id != bot.user.id:
             await message.channel.send("and deceit")
 
         if message_string.strip()[-11:] == "nukes ready" and author_id != bot.user.id:
             await message.channel.send("it's time boys")
             
-        n1 = random.randint(1, 100)
-        if n1 == 69 and author_id == 420348710095159317:
+        n = random.randint(1, 100)
+        if n == 69 and author_id == 420348710095159317:
             await message.channel.send("Kaitlyn is a furry, change my mind.")
 
-        n2 = random.randint(1, 20)
-        if n2 == 1:
+        if n == 1:
             await message.channel.send("King Dragon sends his regards.")
 
-        if n2 == 2:
+        if n == 2:
             await message.channel.send("*you right*")
 
     # @mentions == (mention = f"<@!{author_id}>")
